6092b1850bd6fc4ecb28dc36127f0b7e6f6d3f20
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2022-02-16 16:49:59 +0900

    削除

--- src/utils/api.ts
+++
@@ -1,7 +0,0 @@
-export async function fetcher(url: string, token: string | undefined) {
-  if (token === undefined) {
-    return null;
-  }
-  const response = await fetch(`${url}?accessToken=${token}`);
-  return response.json();
-}
